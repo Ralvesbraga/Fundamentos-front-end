613f1f3b200675094c436cbb701839376cbcca64
Rafael Braga <ralvesbraga40@gmail.com>
2024-09-16 18:16:00 -0500

    iniciando com javascript

---
+++ exemplos/script.js
@@ -0,0 +1,15 @@
+let botao = document.querySelector('button');
+botao.onclick = ()=> {
+    alert('Forma externa de JS');
+}
+botao.addEventListener('click', mensagem);
+botao.addEventListener('click', alteraTitulo);
+function mensagem(){
+    alert('Forma com manipulador de evento JS');
+}
+
+function alteraTitulo(){
+    let titulo = document.querySelector('h1');
+    titulo.innerHTML = 'Web Academy';
+    titulo.style.color = 'red';
+}

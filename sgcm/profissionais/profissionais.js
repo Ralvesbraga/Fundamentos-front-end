52b51d8e9b6d6ea0bc707008c53b851224ee2931
Ralvesbraga <ralvesbraga40@gmail.com>
2024-09-20 12:16:36 -0500

    Adicionando funcionalidade ao botão editar, preenchendo os select unidades e especialidades

--- sgcm/profissionais/profissionais.js
+++ sgcm/profissionais/profissionais.js
@@ -1,16 +1,3 @@
-carregaProfissionais = () =>{
-    let url = "https://my-json-server.typicode.com/juniorlimeiras/json/profissionais"; //armazena a url do servidor que vamos acessar usando o xhr
-    fetch(url).then(resposta => {
-        return resposta.json()
-    }).then(dados =>{
-        for(const profisional of dados){
-            inserirProfissional(profisional);
-        }
-        editarLinha();
-        excluirLinha();
-        quantidadeProfissionais();
-    });
-}
 //     let xhr = new XMLHttpRequest; // instancia o objeto que pede a requisição pro site
 //     xhr.open('GET', url);// Usa o método get para requisitar os dados da url
 //     let tabela = document.querySelector('table');
@@ -26,10 +13,91 @@ carregaProfissionais = () =>{
 //     xhr.send();
 
 // };
+
+carregaProfissionais = () =>{
+    let url = "https://my-json-server.typicode.com/juniorlimeiras/json/profissionais"; //armazena a url do servidor que vamos acessar usando o xhr
+    fetch(url).then(resposta => {
+        return resposta.json()
+    }).then(dados =>{
+        for(const profisional of dados){
+            inserirProfissional(profisional);
+        }
+        editarLinha();
+        excluirLinha();
+        quantidadeProfissionais();
+    });
+}
+
+carregaUnidades = () =>{
+    let url = "https://my-json-server.typicode.com/juniorlimeiras/json/unidades";
+    fetch(url).then(resposta => {
+        return resposta.json()
+    }).then(dados=>{
+        for(let unidade of dados){
+            let option = document.createElement('option');
+            option.value = unidade.id;
+            option.text = unidade.nome;
+            form.unidade.appendChild(option);
+        }
+    })
+}
+
+carregaEspecialidades = () =>{
+    let url = "https://my-json-server.typicode.com/juniorlimeiras/json/especialidades";
+    fetch(url).then(resposta =>{
+        return resposta.json()
+    }).then(dados=>{
+        for(especialidade of dados){
+            let option = document.createElement('option');
+            option.value = especialidade.id;
+            option.text = especialidade.nome;
+            form.especialidade.appendChild(option);
+        }
+    })
+}   
+
 carregaProfissionais();
+carregaUnidades();
+carregaEspecialidades();
+
+//Função que recebe um objeto do tipo profissional
+const inserirProfissional = (profissional) =>{
+    // Criação dos elementos HTML
+    let linha = document.createElement('tr');
+    let id = document.createElement('td');
+    let nome = document.createElement('td');
+    let registro = document.createElement('td');
+    let telefone = document.createElement('td');
+    let email  = document.createElement('td');
+    let especialidade = document.createElement('td');
+    let unidade = document.createElement('td');
+    let opcoes = document.createElement('td');
+    //Preenchendo os elementos HTML ('td')
+    id.textContent =  profissional.id;   
+    nome.textContent =  profissional.nome;   
+    registro.textContent =  profissional.registro;   
+    telefone.textContent = profissional.telefone;   
+    email.textContent  =  profissional.email;   
+    especialidade.textContent =  profissional.especialidade;   
+    unidade.textContent =  profissional.unidade;
+    opcoes.innerHTML = `<a  class="botao_verde"href="javascript:void(0)">Editar</a> <a class="botao_vermelho"href="javascript:void(0)">Excluir</a>`;
+    //Preenchendo a linha ("tr")
+    linha.appendChild(id);
+    linha.appendChild(nome);
+    linha.appendChild(registro);
+    linha.appendChild(telefone);
+    linha.appendChild(email);
+    linha.appendChild(unidade);
+    linha.appendChild(especialidade);
+    linha.appendChild(opcoes);
+    tabela.tBodies[0].appendChild(linha); //Preenchendo a tabela
+};
+
+
 //Inserindo Profissional
 let form = document.querySelector('form');
 let tabela = document.querySelector('table')
+
 form.addEventListener('submit', (evento) => {
     evento.preventDefault(); //Evita que a página seja recarregada.
     objeto ={
@@ -50,42 +118,11 @@ form.addEventListener('submit', (evento) => {
     div.classList.add('inativo');
 });
 
-
-
-//Função que recebe um objeto do tipo profissional
-const inserirProfissional = (profissional) =>{
-        // Criação dos elementos HTML
-        let linha = document.createElement('tr');
-        let id = document.createElement('td');
-        let nome = document.createElement('td');
-        let registro = document.createElement('td');
-        let telefone = document.createElement('td');
-        let email  = document.createElement('td');
-        let especialidade = document.createElement('td');
-        let unidade = document.createElement('td');
-        let opcoes = document.createElement('td');
-        //Preenchendo os elementos HTML ('td')
-        id.textContent =  profissional.id;   
-        nome.textContent =  profissional.nome;   
-        registro.textContent =  profissional.registro;   
-        telefone.textContent = profissional.telefone;   
-        email.textContent  =  profissional.email;   
-        especialidade.textContent =  profissional.especialidade;   
-        unidade.textContent =  profissional.unidade;
-        opcoes.innerHTML = `<a  class="botao_verde"href="javascript:void(0)">Editar</a> <a class="botao_vermelho"href="javascript:void(0)">Excluir</a>`;
-        //Preenchendo a linha ("tr")
-        linha.appendChild(id);
-        linha.appendChild(nome);
-        linha.appendChild(registro);
-        linha.appendChild(telefone);
-        linha.appendChild(email);
-        linha.appendChild(unidade);
-        linha.appendChild(especialidade);
-        linha.appendChild(opcoes);
-        tabela.tBodies[0].appendChild(linha); //Preenchendo a tabela
-};
-
-
+// Função que atualiza a quantidade de profissionais na tfoot
+function quantidadeProfissionais(){
+    foot = document.querySelector('#quantidadeProfissionais');
+    foot.textContent = 'Quantidade de Profissionais: ' + (tabela.getElementsByTagName('tr').length - 2);
+}
 
 
 //Botão Adicionar
@@ -99,7 +136,6 @@ botaoAdicionar.addEventListener('click', ()=> {
 });
 
 //Botão Cancelar
-
 let botaoCancelar = document.querySelector('input[type="button"]');
 botaoCancelar.addEventListener('click', () => {
     div.classList.add('inativo');
@@ -109,7 +145,7 @@ botaoCancelar.addEventListener('click', () => {
 
 //Botão Excluir
 function excluirLinha() {
-    botoes = document.querySelectorAll('a.botao_vermelho')
+    botoes = document.querySelectorAll('a.botao_vermelho');
     for (const botao of botoes) {
         botao.addEventListener('click', () => {
             botao.parentNode.parentNode.remove();
@@ -118,31 +154,59 @@ function excluirLinha() {
     }
 }
 
-function quantidadeProfissionais(){
-    foot = document.querySelector('#quantidadeProfissionais');
-    foot.textContent = 'Quantidade de Profissionais: ' + (tabela.getElementsByTagName('tr').length - 2);
+//Botão Editar
+function editarLinha(){
+    botoes = document.querySelectorAll('td a.botao_verde');
+    for(let botao of botoes){
+        botao.addEventListener('click', ()=> {
+            //Pega a linha da tabela que está sendo editada
+            let linha = botao.parentNode.parentNode;
+            //Pega os campos da linha
+            let dados = linha.getElementsByTagName('td');
+
+            // Armazena em cada variavel o texto do campo correspondente
+            let nome = dados[1].textContent; 
+            let registro = dados[2].textContent; 
+            let telefone = dados[3].textContent; 
+            let email = dados[4].textContent;
+            let unidade = dados[5].textContent;
+            let especialidade = dados[6].textContent;
+
+            // Chama a função para pegar os índices contidos nos respectivos campos
+            let indices = retorna_indice(unidade, especialidade);
+
+            // Modifica o form com o valor dos campos da linha
+            document.getElementById('nome').value = nome;
+            document.getElementById('registroConselho').value = registro;
+            document.getElementById('telefone').value = telefone;
+            document.getElementById('email').value = email;
+            document.getElementById('unidade').selectedIndex = +indices[0];
+            document.getElementById('especialidade').selectedIndex = +indices[1];
+            //Remove a linha
+            linha.remove();
+            //Mostra o fomulário
+            div.classList.remove('inativo');
+            //
+            botaoAdicionar.classList.add('inativo');
+        });
+    }
 }
 
-
-
-function editarLinha() {
-    botoesEditar = document.querySelectorAll('a.botao_verde');
-    for(let botao of botoesEditar){
-        botao.addEventListener('click',()=>{
-            tupla = botao.parentNode.parentNode.getElementsByTagName('td');
-            form.nome.textContent = tupla[1]
-            form.registroConselho.textContent = tupla[2]
-            form.telefone.textContent = tupla[3]
-            form.email.textContent = tupla[4]
-
-            buscarSelect(tupla[5], tupla[6]);
-        })
+function retorna_indice(unidade, especialidade){
+    let unidadeOp = document.getElementById('unidade').options;
+    let idUnidade = 0;
+    for (let option of unidadeOp) {
+        if (option.text === unidade) {
+            idUnidade = option.value;
+        }
+    }
+    let especialidadeOp = document.getElementById('especialidade').options;
+    let idEspecialidade = 0;
+    for (let option of especialidadeOp) {
+        if (option.text === especialidade) {
+            idEspecialidade = option.value;
+        }
     }
 
-}
-
-function buscarSelect(unidade, especialidade){
-    let selectUnidade = form.options;
-    console.log(selectUnidade[1])
-  
+    return [idUnidade, idEspecialidade];
 }
